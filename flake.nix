31419fe41bfe8a07fa60ef0630758c078378f3f8
Alexey Lebedeff <binarin@binarin.info>
2024-12-04 17:02:34 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-parts.url = "flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+  };
+
+  outputs = inputs:
+    let
+      systems = [ "x86_64-linux" ];
+    in inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      inherit systems;
+      imports = [
+      ];
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          name = "nixos-unified-template-shell";
+          meta.description = "Shell environment for modifying this Nix configuration";
+          packages = with pkgs; [
+            clang
+            clangd
+            bazel
+          ];
+        };
+      };
+    };
+}
