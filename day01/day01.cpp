af7d41e50b01ef99ddc49a72ef03a1bffa59015c
Alexey Lebedeff <binarin@binarin.info>
2024-12-04 20:46:13 +0000

    day01p1

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -1,14 +1,18 @@
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 #include <ostream>
 #include <vector>
+#include <ranges>
 
 
 int main(int argc, char **argv) {
   std::vector<int> l1 {};
   std::vector<int> l2 {};
+
   while (!std::cin.eof()) {
     int x;
+
     std::cin >> x;
     l1.push_back(x);
 
@@ -18,4 +22,12 @@ int main(int argc, char **argv) {
 
   std::sort(l1.begin(), l1.end());
   std::sort(l2.begin(), l2.end());
+
+  auto diff = [](auto a, auto b) {
+    return abs(a - b);
+  };
+
+  auto pairs = std::views::zip_transform(diff, l1, l2);
+  auto result = std::reduce(pairs.begin(), pairs.end());
+  std::cout << result << std::endl;
 }
