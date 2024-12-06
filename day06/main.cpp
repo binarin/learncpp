b87c421b016ff35856a325d8daca4b83a1fa14c3
Alexey Lebedeff <binarin@binarin.info>
2024-12-06 11:06:06 +0100

    day06 boilerplate

---
+++ day06/main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <numeric>
+#include <ostream>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <ranges>
+#include <set>
+#include "lib/lib.hpp"
+
+int main(int argc, char **argv) {
+}
