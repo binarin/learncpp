4bf631d2b13bf71e854cddbac9e7f420c3e9f89d
Alexey Lebedeff <binarin@binarin.info>
2024-12-11 10:24:46 +0100

    day10 - p2

--- day10/main.cpp
+++ day10/main.cpp
@@ -11,7 +11,15 @@
 #include <string>
 #include <thread>
 
-bool DEBUG = false;
+[[maybe_unused]] constexpr uint32_t DEBUG_SLOW_RENDER{1 << 0};
+[[maybe_unused]] constexpr uint32_t DEBUG_PRINT_TRAILHEAD_SCORE{1 << 1};
+constexpr uint32_t DEBUG = 0;
+
+template <uint32_t flags, typename T> constexpr void debug_if(T fn) {
+  if constexpr (DEBUG & flags) {
+    fn();
+  }
+}
 
 typedef std::map<Coord2D, int> HeightMap;
 
@@ -32,14 +40,15 @@ std::tuple<Coord2D, Coord2D> bounding_rect(const HeightMap &heights) {
 }
 
 
-int trailhead_score(Coord2D root, const HeightMap &heights) {
+std::pair<int, int> trailhead_score(Coord2D root, const HeightMap &heights) {
   std::set<Coord2D> visited{};
   std::queue<std::pair<Coord2D, int>> queue;
   std::set<Coord2D> queued_coords{};
   std::set<Coord2D> climaxes{};
+  int score = 0;
 
-  auto [minCoord, maxCoord] = bounding_rect(heights);
   auto render_path = [&]() {
+    static auto [minCoord, maxCoord] = bounding_rect(heights);
     using namespace termcolor;
     std::cout << "\033[H\033[2J";
     for (int y = minCoord.y; y <= maxCoord.y; y++) {
@@ -64,15 +73,16 @@ int trailhead_score(Coord2D root, const HeightMap &heights) {
       }
       std::cout << "\n";
     }
+    std::println("\nScore: {}, rating: {}", score, climaxes.size());
     std::cout << std::endl;
   };
 
   queue.emplace(root, 0);
   while (!queue.empty()) {
-    if (DEBUG) {
+    debug_if<DEBUG_SLOW_RENDER>([&](){
       render_path();
       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+    });
 
     auto [curCoord, expectedHeight] = queue.front();
     queue.pop();
@@ -82,14 +92,15 @@ int trailhead_score(Coord2D root, const HeightMap &heights) {
 
     if (heights.at(curCoord) == 9) {
       climaxes.insert(curCoord);
+      ++score;
       continue;
     }
 
     auto maybeAdd = [&](Coord2D c) {
       if (!heights.contains(c))
         return;
-      if (visited.contains(c))
-        return;
+      // if (visited.contains(c))
+      //   return;
       if (heights.at(c) != expectedHeight + 1)
         return;
       queue.emplace(c, expectedHeight + 1);
@@ -102,7 +113,13 @@ int trailhead_score(Coord2D root, const HeightMap &heights) {
     maybeAdd(curCoord.right());
   }
 
-  return climaxes.size();
+  debug_if<DEBUG_SLOW_RENDER>([&]() {
+    render_path();
+    std::println("Done!");
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+  });
+
+  return {climaxes.size(), score};
 }
 
 
@@ -130,14 +147,21 @@ int main(int argc, char **argv) {
     ++y;
   }
 
-  int total{0};
-  if (DEBUG) std::println("Num trailheads {}", roots.size());
+  debug_if<DEBUG_PRINT_TRAILHEAD_SCORE>([&]() {
+    std::println("Num trailheads {}", roots.size());
+  });
+
+  int total_score{0}, total_rating{0};
   for (auto root : roots) {
-    int score = trailhead_score(root, heights);
-    if (DEBUG) std::println("Trailhead ({}, {}) score is {}", root.x, root.y, score);
-    total += score;
+    auto [score, rating] = trailhead_score(root, heights);
+    debug_if<DEBUG_PRINT_TRAILHEAD_SCORE>([&]() {
+      std::println("Trailhead ({}, {}) score is {}, rating is {}", root.x, root.y, score, rating);
+    });
+    total_score += score;
+    total_rating += rating;
   }
-  std::println("Sum all trailheads {}", total);
+  std::println("Score all trailheads {}", total_score);
+  std::println("Rating all trailheads {}", total_rating);
 
   return 0;
 }
